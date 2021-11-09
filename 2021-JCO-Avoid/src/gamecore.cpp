f1c287ff38cde709108f27d2d06db9f2461de4a0
Fabrice <70132110+bricefab1233@users.noreply.github.com>
2021-11-09 14:40:43 +0100

    insérer timer pour le relancement du jeu

--- 2021-JCO-Avoid/src/gamecore.cpp
+++ 2021-JCO-Avoid/src/gamecore.cpp
@@ -77,12 +77,9 @@ GameCore::GameCore(GameCanvas* pGameCanvas, QObject* pParent) : QObject(pParent)
     std::srand(std::time(nullptr));
     // Instancier et initialiser les sprite ici :
 
-
-
     setupPlayer();
     startSpawnObstacleTimer();
     startRetournerEcran();
-
     // Démarre le tick pour que les animations qui en dépendent fonctionnent correctement.
     // Attention : il est important que l'enclenchement du tick soit fait vers la fin de cette fonction,
     // sinon le temps passé jusqu'au premier tick (ElapsedTime) peut être élevé et provoquer de gros
@@ -105,7 +102,7 @@ void GameCore::setupObstacle(){
         setupBonus();
         nombreObstacle = 0;
     }else{
-        Sprite* pObstacle = new Sprite(GameFramework::imagesPath() + "obstacle.png");
+        pObstacle = new Sprite(GameFramework::imagesPath() + "obstacle.png");
         pObstacle->setPos(nbGen,0);
         pObstacle->setScale(0.1);
 
@@ -126,7 +123,7 @@ void GameCore::setupObstacle(){
 }
 
 void GameCore::setupBonus(){
-    Sprite* pBonus = new Sprite(GameFramework::imagesPath() + "Bonus1.png");
+    pBonus = new Sprite(GameFramework::imagesPath() + "Bonus1.png");
     pBonus->setPos(nbGen,0);
     pBonus->setScale(0.25);
 
@@ -181,6 +178,7 @@ void GameCore::setupPlayer() {
     connect(this, &GameCore::notifyKeyPressed, pPlayer, &Player::onKeyPressed);
     connect(this, &GameCore::notifyKeyReleased, pPlayer, &Player::onKeyReleased);
     connect(pPlayer,&Player::onplayerDestroyed, this, &GameCore::stopGame);
+    connect(pPlayer,&Player::onplayerDestroyed, this, &GameCore::restartGame);
     m_pPlayer = pPlayer;
 }
 
@@ -192,7 +190,15 @@ void GameCore::stopGame(){
     m_tickTimerRetournement.stop();
     keyboardDisabled= true;
     m_pGameCanvas->stopTick();
+
+
 }
+
+void GameCore::restartGame(){
+    setupPlayer();
+
+}
+
 
 
 
